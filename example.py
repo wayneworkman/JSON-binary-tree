5fe18f9b5fed65196b820e05a39be4671f389515
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-12-03 22:40:02 +0000

    Added some thoughts on how to generate the equations in JSON format AND they be valid ones too.

--- example.py
+++ example.py
@@ -2,7 +2,28 @@
 import json
 
 
-object = {"+": [5, {"-":[9,8]}]}
+
+keys = {}
+for i in range(,):
+    chars.append(str(chr(i)))
+keyLength = len(keys) - 1
+
+
+digits = []
+for i in range(40,57):
+    digits.append(str(chr(i)))
+digitLength = len(digits) - 1
+
+
+
+
+# start at the top, treat every level the same.
+# Decide randomly - is this a key or a digit?
+# If it's a digit, it's tree ends there. If it's a key, it must produce underneath itself.
+# we can change the probability of key vs digit by weighing keys more heavily, so we get larger equations.
+
+
+object = {"+": [5, {"-":[9,'-']}]}
 
 
 def iterate(object):
@@ -23,5 +44,8 @@ def iterate(object):
 print 'JSON problem: ' + json.dumps(object)
 math = iterate(object)
 print 'Problem: ' + math
-print 'Solution: ' + str(eval(math))
+try:
+    print 'Solution: ' + str(eval(math))
+except:
+    print 'Invalid or malformed mathmatical problem'
 
